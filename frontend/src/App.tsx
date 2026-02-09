29f08515e927ea0e876cc97602120e5214de2d7c
rafaelurben <github@rafaelurben.ch>
2026-02-10 00:03:59 +0100

    setup: Add prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,19 @@
-import { Link, Route, Routes } from "react-router-dom"
-import './App.css'
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
 
 export default function App() {
-    return (
-        <>
-            <nav>
-                <Link to="/">Home</Link> |{" "}
-                <Link to="/about">About</Link>
-            </nav>
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
 
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-            </Routes>
-        </>
-    )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 }
