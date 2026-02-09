dee583381ed46801a92d8bb57d01f2688b903282
rafaelurben <github@rafaelurben.ch>
2026-02-10 00:28:03 +0100

    setup: Add shadcn ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,21 @@
 import { Link, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
+import Login from "@/pages/Login.tsx";
 
 export default function App() {
   return (
-    <>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </>
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
