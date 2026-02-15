db70b71909bdd9d5758c8e18a7e087155bcb9989
rafaelurben <github@rafaelurben.ch>
2026-02-15 15:27:08 +0100

    feat(backend): Add basic security config

---
+++ backend/src/main/java/ch/rafaelurben/sheetmusiclearner/backend/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+/* (C) 2026 - Rafael Urben */
+package ch.rafaelurben.sheetmusiclearner.backend.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) {
+    http.oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+        .authorizeHttpRequests(
+            auth ->
+                auth.requestMatchers("/api/public/**")
+                    .permitAll()
+                    .requestMatchers("/actuator/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated());
+    return http.build();
+  }
+}
